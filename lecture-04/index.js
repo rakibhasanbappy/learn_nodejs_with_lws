5722d204ae5c3775416876ba922080007b4ce5cb
Rakib Hasan Bappy <rakibhasan15144@gmail.com>
2023-12-06 21:17:02 +0600

    lecture-04 added

---
+++ lecture-04/index.js
@@ -0,0 +1,13 @@
+// Lecture #4: Here I learned about how to export and import user-defined
+// modules in Node.js, and how to use the npm/yarn module(lodash).
+
+const _ = require('lodash');
+const people = require('./people');
+
+console.log(_.last(people.people));
+
+console.log(people.say());
+
+console.log(_.reverse(people.people));
+
+console.log(people.a);
